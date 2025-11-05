616cc0b9fc10b8bd0a8ce84b57bbeeb84de68390
Celeste Antenao Fernandez <cele.antenao@gmail.com>
2025-11-05 15:15:19 -0300

    cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 //CORS
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:5173/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
