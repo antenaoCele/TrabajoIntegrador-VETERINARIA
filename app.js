9d27f1ac43b00d3ccf8aba3351a8c3606681c62c
Celeste Antenao Fernandez <cele.antenao@gmail.com>
2025-11-05 15:46:34 -0300

    Modificando app.js

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ const mascotaRoutes = require('./routes/mascotas');
 
 const app = express();
 
-//CORS
+// CORS
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-
 
 app.use(express.json());
 
@@ -23,15 +22,9 @@ connectDB();
 app.use('/clientes', clienteRoutes);
 app.use('/mascotas', mascotaRoutes);
 
-
 app.get('/', (req, res) => {
     res.send('API de Veterinaria funcionando');
 });
 
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
-});
-
-module.exports = app; 
+module.exports = app;
